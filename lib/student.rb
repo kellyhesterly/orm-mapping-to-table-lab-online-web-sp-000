73bf9c3fa04d9d87e5462e4856d3dfbe76edd1c5
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-07 20:36:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,5 +13,12 @@ class Student
   end
 
   def self.create_table
+    sql = <<-SQL
+      CREATE TABLE IF NOT EXISTS students (
+        id INTEGER PRIMARY KEY,
+        name TEXT,
+        grade INTEGER
+      );
+    SQL
   end
 end
